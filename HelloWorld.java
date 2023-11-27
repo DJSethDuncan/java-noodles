065a5ee1f5851ddbbe0154c5973d6c69ae8997bf
Seth Duncan <sethduncan@gmail.com>
2023-11-26 22:10:28 -0600

    add some complexity

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
+
 class HelloWorld {
   public static void main(String[] arguments) {
-    printHelloWorld("Hello Worlds!");
+    showGreeting(true);
+    String userName = getUsername();
+    System.out.println("Hello, " + userName);
   }
 
-  static void printHelloWorld(String arguments) {
-    System.out.println(arguments + " Yay!");
+  static void showGreeting(Boolean firstVisit) {
+    String welcomeText;
+    if (firstVisit) 
+      welcomeText = "Welcome to Java Program";
+    else
+      welcomeText = "Welcome back, friendo";
+
+    System.out.println(welcomeText);
+  }
+
+  static String getUsername() {
+    Scanner scnr = new Scanner(System.in);
+    System.out.print("Username: ");
+    return scnr.nextLine();
   }
 }
